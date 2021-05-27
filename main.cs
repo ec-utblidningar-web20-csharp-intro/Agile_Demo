2b5152424459334f2852793845b1125c4f480ece
Björn Strömberg <bjorn.stromberg@codic.se>
2021-05-27 10:37:05 +0200

    Nu funkar allt

--- main.cs
+++ main.cs
@@ -1,26 +1,19 @@
 using System.IO;
-//ojojojoj
-void main() {
 
-int counter = 1;
-while (true)
+void main()
 {
-    Console.Write("{0}", counter);
-    counter++;
+    int counter = 1;
+    while (true)
+    {
+        Console.Write("{0}", counter);
+        counter++;
+
+        Console.Write("Hello from Johanna");
+
+        int hej = 5;
+        Console.Log(hej);
+
+        int things = 9000;
+        double moon = 420,69;
+    }
 }
-
-
-   Console.Write("Hello from Johanna");
-
-  int hej = 5;
-  Console.Log(hej);
-  
-  int things = 9000;
-
-
-
-
-  double moon = 420,69
-
-}
-//hejhehejhej
