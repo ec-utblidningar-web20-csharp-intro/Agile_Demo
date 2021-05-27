baf03d9f77f2d142ca8d763295dc736a181d3510
Jack Gelotte <70569401+JackGelotte@users.noreply.github.com>
2021-05-27 13:35:02 +0200

    issue - Anxiety
    stuff osv.

--- main.cs
+++ main.cs
@@ -10,10 +10,10 @@ void main()
 
         Console.Write("Hello from Johanna");
 
-        int hej = 5;
+        int nop = 3;
         Console.Log(hej);
 
-        int things = 9000;
-        double moon = 420,69;
+        int stuff = 42;
+        double yup = 420;
     }
 }
